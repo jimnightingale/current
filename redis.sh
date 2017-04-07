44bd156b0615a466010a775ddc94ac340aa8d06e
jimnightingale <nightingalejames@yahoo.co.uk>
2017-04-07 16:01:32 +0100

    Update redis.sh

--- redis.sh
+++ redis.sh
@@ -15,13 +15,13 @@ sudo adduser --system --user-group --no-create-home redis
 
 
 #sudo scp $USR@srv:/etc/redis/redis.conf /etc/redis/
-sudo sshpass -p '$PASSWD' scp $USR@srv:/etc/redis/redis.conf /etc/redis/
-sudo sshpass -p '$PASSWD' scp $USR@srv:/etc/systemd/system/redis.service /etc/systemd/system/
-sudo sshpass -p '$PASSWD' scp $USR@srv:/usr/local/bin/redis-benchmark /usr/local/bin/
-sudo sshpass -p '$PASSWD' scp $USR@srv:/usr/local/bin/redis-check-aof /usr/local/bin/
-sudo sshpass -p '$PASSWD' scp $USR@srv:/usr/local/bin/redis-check-rdb /usr/local/bin/
-sudo sshpass -p '$PASSWD' scp $USR@srv:/usr/local/bin/redis-cli /usr/local/bin/
-sudo sshpass -p '$PASSWD' scp $USR@srv:/usr/local/bin/redis-server /usr/local/bin/
+sudo sshpass -p $PASSWD scp $USR@srv:/etc/redis/redis.conf /etc/redis/
+sudo sshpass -p $PASSWD scp $USR@srv:/etc/systemd/system/redis.service /etc/systemd/system/
+sudo sshpass -p $PASSWD scp $USR@srv:/usr/local/bin/redis-benchmark /usr/local/bin/
+sudo sshpass -p $PASSWD scp $USR@srv:/usr/local/bin/redis-check-aof /usr/local/bin/
+sudo sshpass -p $PASSWD scp $USR@srv:/usr/local/bin/redis-check-rdb /usr/local/bin/
+sudo sshpass -p $PASSWD scp $USR@srv:/usr/local/bin/redis-cli /usr/local/bin/
+sudo sshpass -p $PASSWD scp $USR@srv:/usr/local/bin/redis-server /usr/local/bin/
 
 cd /usr/local/bin
 sudo ln -s  redis-server /usr/local/bin/redis-sentinel
